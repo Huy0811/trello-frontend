0bafa38a6c2bebb899b6b3747ce7a68a127ba14a
Huy0811 <duongchihuy.study@gmail.com>
2024-04-17 11:34:13 +0700

    config_mui_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm"
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation"
 import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
 
 function App() {
   return (
     <>
-      <div>trungquandev</div>
+      <Typography variant="body2" color={"text.secondary"}>
+        Huy
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
