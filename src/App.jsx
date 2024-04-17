ac56fea8558704f4eba904bd67f86c9a3b2dbd82
Huy0811 <duongchihuy.study@gmail.com>
2024-04-17 13:49:47 +0700

    dark_light_p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,71 @@
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm"
+import DarkModeIcon from "@mui/icons-material/DarkMode"
+import LightModeIcon from "@mui/icons-material/LightMode"
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness"
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation"
+import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import Select from "@mui/material/Select"
 import { useColorScheme } from "@mui/material/styles"
 import Typography from "@mui/material/Typography"
 
-function ModeToggle() {
+function ModeSelect() {
   const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === "light" ? "dark" : "light")
-      }}
-    >
-      {mode === "light" ? "Turn dark" : "Turn light"}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <LightModeIcon fontSize="small" />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <DarkModeIcon fontSize="small" />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
 function App() {
   return (
     <>
-      <ModeToggle />
+      <ModeSelect />
+      <hr />
       <Typography variant="body2" color={"text.secondary"}>
         Huy
       </Typography>
+      <hr />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <hr />
       <AccessAlarmIcon />
       <ThreeDRotation />
     </>
