a990a7f45fcb5f23f550ad93a57b8079066bd801
Huy0811 <duongchihuy.study@gmail.com>
2024-04-17 12:15:26 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm"
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation"
 import Button from "@mui/material/Button"
+import { useColorScheme } from "@mui/material/styles"
 import Typography from "@mui/material/Typography"
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light")
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
       <Typography variant="body2" color={"text.secondary"}>
         Huy
       </Typography>
